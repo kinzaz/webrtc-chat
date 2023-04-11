5af3ed02f80ef8bd5a7765697648f4833a14a977
Vladislav Vlasenko <kinzazdev579@gmail.com>
2023-04-11 21:46:43 +0300

    video layout

--- src/pages/Room/index.tsx
+++ src/pages/Room/index.tsx
@@ -1,15 +1,56 @@
 import { useParams } from 'react-router';
 import useWebRTC, { LOCAL_VIDEO } from '../../hooks/useWebRTC';
 
+function layout(clientNumber) {
+	const pairs = Array.from({ length: clientNumber }).reduce(
+		(acc, next, index, arr) => {
+			if (index % 2 === 0) {
+				acc.push(arr.slice(index, index + 2));
+			}
+			return acc;
+		},
+		[]
+	);
+	const rowsNumber = pairs.length;
+	const height = `${100 / rowsNumber}%`;
+	return pairs.map((row, index, arr) => {
+		if (index == arr.length - 1 && row.length == 1) {
+			return [
+				{
+					width: '100%',
+					height,
+				},
+			];
+		}
+		return row
+			.map(() => ({
+				width: '50%',
+				height,
+			}))
+			.flat();
+	});
+}
+
 export const Room = (): JSX.Element => {
 	const { id: roomId } = useParams();
 	const { clients, provideMediaRef } = useWebRTC(roomId);
+	const videoLayout = layout(clients.length);
 	return (
-		<div>
-			{clients.map(clientId => {
+		<div
+			style={{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				flexWrap: 'wrap',
+				height: '100vh',
+			}}
+		>
+			{clients.map((clientId, index) => {
 				return (
-					<div key={clientId}>
+					<div key={clientId} style={videoLayout[index]}>
 						<video
+							width={'100%'}
+							height={'100%'}
 							ref={instance => {
 								provideMediaRef(clientId, instance);
 							}}
